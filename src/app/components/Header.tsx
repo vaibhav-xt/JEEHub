970c752ec927d74db45e3a2634fcbfbcdf2b1339
Vaibhav Maurya <vibhu.astute@gmail.com>
2023-12-23 18:25:51 +0530

    added navlink dynamic & removed image

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,9 +1,30 @@
 'use client'
 import React, { useEffect, useState } from 'react';
-import Image from 'next/image';
 import { usePathname } from 'next/navigation';
-import bgImage from '../assets/header-wave.svg';
 import Link from 'next/link';
+
+const NavLink = [
+    {
+        name: 'Home',
+        link: '/'
+    },
+    {
+        name: 'Physics',
+        link: '/physics'
+    },
+    {
+        name: 'Chemistry',
+        link: '/chemistry'
+    },
+    {
+        name: 'Math',
+        link: '/math'
+    },
+    // {
+    //     name: 'About Us',
+    //     link: '/'
+    // }
+]
 
 export default function Header() {
     const pathname = usePathname();
@@ -30,45 +51,16 @@ export default function Header() {
 
                     {/* Nav Links */}
                     <nav className={`transition-all ease-in-out flex flex-col md:gap-8 md:flex-row absolute left-0 right-0 top-16 w-screen overflow-hidden shadow-md md:shadow-none shadow-dark-purple ${displayNav ? 'h-auto p-4 border-t-2' : 'h-0'} bg-dark-purple gap-2 md:relative md:top-0 md:w-auto md:border-none`}>
-
-                        <p>
-                            <Link
-                                href="/"
-                                className={`${pathname === '/' ? 'text-light-purple after:w-full' : ''} relative hover:text-light-purple`}>
-                                Home
-                            </Link>
-                        </p>
-
-                        <p>
-                            <Link
-                                href="/physics"
-                                className={`${pathname === '/physics' ? 'text-light-purple after:w-full' : ''} hover:text-light-purple`}>
-                                Physics
-                            </Link>
-                        </p>
-
-                        <p>
-                            <Link
-                                href="/chemistry"
-                                className={`${pathname === '/chemistry' ? 'text-light-purple after:w-full' : ''} hover:text-light-purple`}>
-                                Chemistry
-                            </Link>
-                        </p>
-
-                        <p>
-                            <Link
-                                href="/maths"
-                                className={`${pathname === '/maths' ? 'text-light-purple after:w-full' : ''} hover:text-light-purple`}>
-                                Maths
-                            </Link>
-                        </p>
-
-                        <p>
-                            <Link
-                                href="/">
-                                About Us
-                            </Link>
-                        </p>
+                        {
+                            NavLink.map((route) => (
+                                <p key={route.name}>
+                                    <Link
+                                        href={route.link}
+                                        className={`${pathname === route.link ? 'text-light-purple after:w-full' : ''} relative hover:text-light-purple`}>
+                                        {route.name}
+                                    </Link>
+                                </p>))
+                        }
                     </nav>
                     <div className='flex gap-4 items-center'>
                         <Link
@@ -83,8 +75,6 @@ export default function Header() {
                     </div>
                 </div>
             </header>
-            {/* Uncomment the following line if you want to use the Image component */}
-            {/* <Image className='w-screen' src={bgImage} alt="bgImage" /> */}
         </>
     );
 }
